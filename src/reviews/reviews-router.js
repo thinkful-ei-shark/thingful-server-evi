701968946ab551fb281dd921fe51fdfc3e9f6e4b
Evisari Riyanto <evisari111012@gmail.com>
2020-11-04 12:01:55 -0800

    finished with assignment

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -7,14 +7,16 @@ const reviewsRouter = express.Router();
 const jsonBodyParser = express.json();
 
 reviewsRouter.route("/").post(requireAuth, jsonBodyParser, (req, res, next) => {
-  const { thing_id, rating, text, user_id } = req.body;
-  const newReview = { thing_id, rating, text, user_id };
+  const { thing_id, rating, text } = req.body;
+  const newReview = { thing_id, rating, text };
 
   for (const [key, value] of Object.entries(newReview))
     if (value == null)
       return res.status(400).json({
         error: `Missing '${key}' in request body`,
       });
+
+  newReview.user_id = req.user.id;
 
   ReviewsService.insertReview(req.app.get("db"), newReview)
     .then((review) => {
